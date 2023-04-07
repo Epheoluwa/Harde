689d359ab0e5574fd94deaa840ce7305cbd83562
Solomon sunmola <sunmolasolomon@gmail.com>
2023-04-07 22:42:12 +0100

    update book record from Frontend

--- app/Http/Controllers/FrontEndBookController.php
+++ app/Http/Controllers/FrontEndBookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FrontEndBookController extends Controller
 {
@@ -21,6 +22,29 @@ class FrontEndBookController extends Controller
             return redirect()->route('index')->with('message','Book record deleted Successfully');
         }else{
             return redirect()->route('index')->with('error','Something went wrong, Please try again');
+        }
+    }
+
+    public function editrecord(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'isbn' => 'required',
+            'authors' => 'required',
+            'country' => 'required',
+            'number_of_pages' => 'required',
+            'publisher' => 'required',
+            'release_date' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('index')->with('error',$validator->errors()->first());
+        }
+        $data = $request->except('_token','_method');
+        $update = Book::whereId($id)->update($data);
+        if($update)
+        {
+            return redirect()->route('index')->with('message','Book record updated Successfully');
         }
     }
 }
